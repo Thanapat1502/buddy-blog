f410df568bb3088a1bb32d14de98483bd2ab83c8
Thanapat1502 <psy150201027@gmail.com>
2025-01-24 00:35:40 +0700

    feat: add carousel

--- components/landing_page_component/HeroSection.vue
+++ components/landing_page_component/HeroSection.vue
@@ -12,10 +12,31 @@
           Inspiration and Information.
         </p>
       </div>
-      <img
-        src=""
-        alt="Person with a cat"
-        class="h-[530px] object-cover rounded-lg shadow-lg lg:w-1/3 mx-4 mb-8 lg:mb-0" />
+      <Carousel>
+    <CarouselContent>
+      <CarouselItem><img
+src="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449784/my-blog-post/xgfy0xnvyemkklcqodkg.jpg"
+alt="Person with a cat"
+class="h-[530px] object-cover rounded-lg shadow-lg lg:w-1/3 mx-4 mb-8 lg:mb-0" /></CarouselItem>
+      <CarouselItem><img
+src="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449784/my-blog-post/xgfy0xnvyemkklcqodkg.jpg"
+alt="Person with a cat"
+class="h-[530px] object-cover rounded-lg shadow-lg lg:w-1/3 mx-4 mb-8 lg:mb-0" /></CarouselItem>
+      <CarouselItem><img
+src="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449784/my-blog-post/xgfy0xnvyemkklcqodkg.jpg"
+alt="Person with a cat"
+class="h-[530px] object-cover rounded-lg shadow-lg lg:w-1/3 mx-4 mb-8 lg:mb-0" /></CarouselItem>
+    </CarouselContent>
+    <CarouselPrevious />
+    <CarouselNext />
+  </Carousel>
+      
+
+  <!-- <img
+src=""
+alt="Person with a cat"
+class="h-[530px] object-cover rounded-lg shadow-lg lg:w-1/3 mx-4 mb-8 lg:mb-0" /> -->
+  
       <div class="lg:w-1/3 lg:pl-8">
         <h2 class="text-xl font-semibold mb-2">-Author</h2>
         <h3 class="text-2xl font-bold mb-4">Thompson P.</h3>
@@ -34,7 +55,9 @@
 </template>
 
 <script>
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
 export default {
   name: "HeroSection",
+  components: {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious}
 };
 </script>
