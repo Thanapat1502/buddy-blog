c3bbf670d6441ee5538543f50576733ec9147aa0
Thanapat1502 <psy150201027@gmail.com>
2025-01-29 07:27:11 +0700

    fix: try to fix home re-render, api is fine, now fix ui

--- components/landing_page_component/ArticleSection.vue
+++ components/landing_page_component/ArticleSection.vue
@@ -16,15 +16,17 @@
         </div>
         <div class="dropdown-container lg:hidden">
           <label htmlFor="category" class="lg:hidden mb-2"> Category </label>
-          <Select onValueChange="{handleSelector}">
+          <Select
+            v-model="selectedCategory"
+            @update:modelValue="handleSelector">
             <SelectTrigger class="w-full h-12 rounded-lg border">
               <SelectValue placeholder="Categories" />
             </SelectTrigger>
             <SelectContent>
               <SelectItem
-                v-for="cate in refCategories"
+                v-for="cate in refCate"
                 :key="cate.id"
-                :value="cate.id">
+                :value="cate.name">
                 {{ cate.name }}
               </SelectItem>
             </SelectContent>
@@ -33,9 +35,14 @@
         <div
           class="category-dropdown-main rounded-lg py-3 px-4 relative hidden lg:flex lg:items-center">
           <button
-            v-for="cate in refCategories"
+            v-for="cate in refCate"
             :key="cate.id"
             :value="cate.id"
+            @click="
+              () => {
+                handleCateClick(cate.name);
+              }
+            "
             class="font-medium text-base rounded-lg hover:bg-[#DAD6D1] active:bg-[#F9F8F6] px-5 py-3">
             {{ cate.name }}
           </button>
@@ -47,15 +54,105 @@
 
   <div
     class="gride-area flex flex-col justify-center items-center px-4 pt-6 pb-20 gap-12 lg:grid lg:grid-cols-2 lg:container lg:mx-auto">
-    <div v-for="post in refPosts">
+    <div v-for="post in refPosts" :key="post.id">
       <ArticleCard :post="post" />
     </div>
   </div>
-  <div></div>
+
   <!--Area -->
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+import { ref, onMounted, reactive } from "vue";
+import axios from "axios";
+import ArticleCard from "@/components/landing_page_component/ArticleCard.vue";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const refPosts = ref([]);
+const refCate = ref([]);
+let postLimit = ref(10);
+let inputKeyword = ref("");
+let inputCate = ref("highlight");
+
+const handleSelector = (selectedValue: string) => {
+  inputCate.value = selectedValue.toLowerCase();
+  console.log(inputCate);
+};
+
+const handleCateClick = (value: string) => {
+  inputCate.value = value.toLowerCase();
+  console.log(inputCate);
+};
+
+const getAllPosts = async () => {
+  try {
+    const respond = await axios.get(`/api/allposts/getposts?limit=10`);
+    refPosts.value = respond.data.data;
+    console.log("posts: ", refPosts);
+  } catch (e) {
+    const error = e as Error;
+    console.log(error.message);
+  }
+};
+
+const getCategories = async () => {
+  try {
+    const cateRes = await axios.get("/api/categories");
+    refCate.value = cateRes.data.categories;
+  } catch (e) {
+    const error = e as Error;
+    console.log(error.message);
+  }
+};
+
+const filterPosts = async (newCate, newKeyword) => {
+  try {
+    console.log("Filtered");
+    let endpoint = ``;
+    if (inputCate.value !== "highlight" && inputKeyword.value.trim() !== "") {
+      console.log(`F Condition I`);
+      endpoint = `/api/allposts/getposts?limit=${postLimit.value}&category=${inputCate.value}&keyword=${inputKeyword.value}`;
+    }
+    if (inputCate.value !== "highlight" && inputKeyword.value.trim() === "") {
+      console.log(`F Condition II`);
+      endpoint = `/api/allposts/getposts?limit=${postLimit.value}&category=${inputCate.value}`;
+    }
+    if (inputCate.value === "highlight" && inputKeyword.value.trim() !== "") {
+      console.log(`F Condition III`);
+      endpoint = `/api/allposts/getposts?limit=${postLimit.value}&keyword=${inputKeyword.value}`;
+    }
+    console.log("endpoint: ");
+    console.log(endpoint);
+    const res = await axios.get(endpoint);
+    refPosts.value = res.data.data;
+    console.log("filtered:", refPosts.value);
+  } catch (e) {
+    const error = e as Error;
+    console.log(error.message);
+  }
+};
+
+onMounted(async () => {
+  getCategories();
+  getAllPosts();
+});
+
+watch(
+  [inputCate, inputKeyword],
+  async ([newCate, newKeyword], [oldCate, oldKeyword]) => {
+    console.log(`Cate change: `, newCate);
+    console.log(`Keyword change: `, newKeyword);
+    filterPosts(newCate, newKeyword);
+  }
+);
+</script>
+<!-- <script lang="ts">
 import {
   Select,
   SelectContent,
@@ -75,8 +172,13 @@ interface Category {
 export default {
   name: "ArticleSection",
   data() {
+    let postCategory = ref(null);
+    let postKeyword = ref(null);
+    let postLimit = ref(10);
     return {
-      mockCatagory: ["Highlight", "Inspiration", "Cat", "Space"],
+      limit: postLimit,
+      keyword: postKeyword,
+      category: postCategory,
     };
   },
   components: {
@@ -93,8 +195,9 @@ export default {
     onMounted(async () => {
       console.log("mounted");
       try {
-        const respond = await axios.get("/api/allposts/getposts");
+        const respond = await axios.get(`/api/allposts/getposts?limit=10`);
         refPosts.value = respond.data.data;
+
         const cateRes = await axios.get("/api/categories");
         refCategories.value = cateRes.data.categories;
         console.log("posts: ", typeof refPosts, refPosts);
@@ -110,5 +213,20 @@ export default {
       refCategories,
     };
   },
+  methods: {
+    async filterPosts() {
+      const postLimit = this.limit;
+      const postCategory = this.category;
+      const postKeyword = this.keyword;
+      const res = await axios.get(
+        `/api/allposts/getposts?limit=${postLimit}&category=${postCategory}&keyword=${postKeyword}`
+      );
+      this.refPosts.values = res.data.data;
+      console.log(
+        `limit: ${postLimit}, cate: ${postCategory}, keyword: ${postKeyword}, res: ${res.data.data}`
+      );
+      console.log("res: ", res.data.data);
+    },
+  },
 };
-</script>
+</script> -->
--- server/api/allposts/getposts.ts
+++ server/api/allposts/getposts.ts
@@ -1,19 +1,57 @@
-import { defineEventHandler } from "h3";
+import { defineEventHandler, getQuery } from "h3";
 import { supabase } from "@/supabaseClient";
 
-export default defineEventHandler(async () => {
-  const { data, error } = await supabase.from("posts").select(`
-      id,
-      image,
-      title,
-      description,
-      date,
-      content,
-      likes_count,
-      categories (name),
-      statuses (status),
-      author,
-      date`);
+export default defineEventHandler(async (event) => {
+  const { category, keyword, limit } = getQuery(event);
+  console.log(`limit: `, limit);
+  console.log(`category: `, category);
+  console.log(`keyword: `, keyword);
+  console.log(`_____________________`);
+  let pageLimit = limit;
+  if (!limit || Number(limit) < 1) {
+    pageLimit = 10;
+  }
+  console.log("pageLimit: ", pageLimit);
+  let query = supabase
+    .from("posts")
+    .select(
+      `
+    id,
+    image,
+    title,
+    description,
+    date,
+    content,
+    likes_count,
+    categories (name),
+    statuses (status),
+    author,
+    date`
+    )
+    .limit(Number(pageLimit));
+
+  if (category) {
+    console.log(`test I`);
+    query = query.eq("categories.name", category);
+  }
+
+  if (keyword) {
+    console.log(`test II`);
+    query = query.ilike("title", `%${keyword}%`);
+  }
+  if (category && keyword) {
+    console.log(`test III`);
+    query = query
+      .ilike("title", `%${keyword}%`)
+      .eq("categories.name", category);
+  }
+  console.log("q3: ", query);
+
+  const { data, error } = await query;
+  // console.log("**********_____________**************");
+  // console.log("data:");
+  // console.log(data);
+  // console.log("**********_____________**************");
 
   if (error) {
     return { success: false, message: error.message };
@@ -21,3 +59,16 @@ export default defineEventHandler(async () => {
 
   return { success: true, data };
 });
+
+// if (category && keyword) {
+//   query = query
+//     .ilike("title", `%${keyword}%`)
+//     .eq("categories.name", category);
+// }
+// if (category) {
+//   query = query.eq("categories.name", category);
+// }
+// if (keyword) {
+//   query = query.ilike("title", `%${keyword}%`);
+// }
+// query = query.limit(limit ?? 10);
